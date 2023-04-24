2bfb907d6d84b18acc8322be62b1ba9575d2b8ad
ponlakrit1 <ponlakritkm@gmail.com>
2023-04-24 16:26:43 +0700

    MODIFY - change screen header

--- lib/screens/like_screen.dart
+++ lib/screens/like_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/style.dart';
+
 class LikeScreen extends StatefulWidget {
   const LikeScreen({Key? key}) : super(key: key);
 
@@ -17,6 +19,23 @@ class _LikeScreenState extends State<LikeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: const Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Text('Profile', style: kTextStyleHeaderPrimary,),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(IconData(0xe904, fontFamily: 'messageNav')),
+                iconSize: 20.0,
+                color: Colors.black,
+                onPressed: () {
+
+                },
+              )
+            ]
+        ),
         body: SafeArea(
           child: Container()
         )
